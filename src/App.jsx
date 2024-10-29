7b2155f3421224ac4c2336bc3f62084259305b9c
obia isreal ogonna <obiaisreal875@gmail.com>
2024-10-29 05:59:53 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Home from './components/Home'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div>
+    hello from other side
+  </div>
+  )
+}
+
+export default App
---
+++ src/component/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+
+    return (
+        <div>
+            <h1>Home page</h1>
+            <p>welcome to home page!</p>
+        </div>
+    )
+}
+
+export default Home;
